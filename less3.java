e2c76ab1abf19f3f18c5b53485e79de6a20b5e08
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-23 22:01:23 +0200

    Вперед назад

---
+++ less3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class less3 {
+
+    public static void main(String args[]){
+
+        Scanner console = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = console.nextInt();
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
+}
