a6f72f7629edde5aeb72561f3202ff0b8c9e4bac
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-23 21:38:29 +0200

    масиви осн

---
+++ less2.java
@@ -0,0 +1,12 @@
+class less2 {
+
+    public static void main(String args[]){
+
+        int [] array = new int[100];
+        array [2] = 50;
+        array [5] = 85;
+        array [7] = array [2] + array [5];
+
+        System.out.println(array [7]);
+    }
+}
