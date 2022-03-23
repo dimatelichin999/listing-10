84717487618123b53bdd8e5644d018f303d5f487
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-23 22:11:19 +0200

    Мінімальне значення зі чисел які ведені

---
+++ less4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class less4 {
+
+    public static void main (String args[]){
+
+        Scanner console = new Scanner(System.in);
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = console.nextInt();
+        }
+        int min = array[0];
+
+        for (int i = 1; i < 10; i++) {
+            if (array[i] < min)
+
+                min = array[i];
+        }
+        System.out.println(min);
+    }
+}
