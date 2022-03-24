756c828ee6d99deab7d71884187ad3583d99ab75
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-24 13:01:32 +0200

    массиви завд які не пон
    1.сер зн - не розв
    2.дійсність елемента
    3.знаходження адреса елемента

---
+++ less8.java
@@ -0,0 +1,23 @@
+class less8 {
+
+    public static void main(String args[]) {
+
+        int[] a = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int sum = 0;
+
+        int i;
+        for (i = 0; i < a.length; i++) ;
+        sum += a[i];
+
+        double b = sum /a.length;
+
+
+
+
+
+
+        System.out.println("Середнє число " + b);
+
+    }
+}
